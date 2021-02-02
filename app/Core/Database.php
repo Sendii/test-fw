a28afd934da9c72a64d1c29813cd19bff5ad5709
Sendii <sendidianhadiwijaya@gmail.com>
2021-02-02 11:59:34 +0700

    Add arr to connect db

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -1,14 +1,16 @@
 <?php
 
-class Database{
+class Database{	
 	protected $mysqli;
-	protected $hostname  = 'localhost';
-	protected $username  = 'root';
-	protected $password  = '';
-	protected $database  = 'test-fw';
 
 	public function __construct(){
-		$this->mysqli = new mysqli($this->hostname, $this->username, $this->password, $this->database);
+		$connect   = require_once '../config/app.php';
+		$hostname  = ( !empty($connect['database']['mysql']['hostname']) ? $connect['database']['mysql']['hostname'] : 'localhost' );
+		$username  = ( !empty($connect['database']['mysql']['username']) ? $connect['database']['mysql']['username'] : 'root' );
+		$password  = ( !empty($connect['database']['mysql']['password']) ? $connect['database']['mysql']['password'] : '' );
+		$database  = ( !empty($connect['database']['mysql']['database']) ? $connect['database']['mysql']['database'] : 'my_database' );
+
+		$this->mysqli = new mysqli($hostname, $username, $password, $database);
 
 		if ($this->mysqli->connect_errno) {
 			echo "Failed to connect to MySQL: " . $this->mysqli->connect_error;
---
+++ config/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'database' => [
+		'mysql' => [
+			'hostname' => 'localhost',
+			'username' => 'root',
+			'password' => '',
+			'database' => 'test-fw'
+		]
+	]
+];
