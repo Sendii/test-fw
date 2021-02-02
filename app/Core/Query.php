8d574fd7b41bba2594c6894d50d3964954823719
Sendii <sendidianhadiwijaya@gmail.com>
2021-02-02 17:00:23 +0700

    Add if table not exists

--- app/Core/Query.php
+++ app/Core/Query.php
@@ -1,14 +1,34 @@
 <?php
 
 class Query extends Database{
+	public $className;
+	public $tableName;
+
+	function getNameTable(){
+		$this->className = get_called_class();
+		$this->tableName = get_called_class();
+
+		require_once '../app/Models/'.$this->className.'.php';
+		if(property_exists($this->className, 'table_name')){
+			$class = new $this->className;
+			$this->tableName = $class->table_name;
+		}
+		return $this->tableName;
+	}
 
 	public function index(){
+		// var_dump($this->getNameTable());
 		$mysqli = new mysqli('localhost', 'root', '', 'test-fw');
 		$reply = [];
-		$result = $mysqli->query('SELECT * FROM '.get_called_class());
+		if ($mysqli->query('DESCRIBE '.$this->getNameTable())) {
+			$result = $mysqli->query('SELECT * FROM '.$this->getNameTable());
+			while ($row = $result->fetch_assoc())
+				$reply[] = $row;		
 
-		while ($row = $result->fetch_assoc())
-			$reply[] = $row;
-		var_dump($reply);
+			var_dump($reply);	
+		}else{
+			echo 'gaada';
+			exit();
+		}		
 	}
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,5 +1,5 @@
 <?php
 
 class User extends Model{
-	
+	protected $table_name = 'user';
 }
